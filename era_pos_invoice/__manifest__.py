82d7b376c8307a170cd0981a8704c33e0552b972
eranetsa <41900050+eranetsa@users.noreply.github.com>
2021-12-15 09:27:20 +0300

    Add files via upload

--- era_pos_invoice/__manifest__.py
+++ era_pos_invoice/__manifest__.py
@@ -15,18 +15,17 @@
     'version': '0.1',
     'license': 'AGPL-3',
     'images': ['static/description/main_screenshot.png'],
-    # 'depends': ['base', 'account', 'point_of_sale'],
-    'depends': ['era_pos_tax_invoice', 'point_of_sale'],
-    "data": [],
+    # 'depends': ['base', 'account', 'point_of_sale', 'era_pos_tax_invoice'],
+    'depends': ['base', 'account', 'point_of_sale'],
+    "data": [
+        'report/pos_report.xml',
+    ],
     'qweb': ['static/src/xml/pos.xml'],
     "application": True,
 
     'assets': {
-        # 'point_of_sale.assets': [
-        #     'era_pos_invoice/static/src/js/pos.js',
-        # ],
         'web.assets_qweb': [
-            'era_pos_invoice/static/src/xml/pos_new.xml',
+            'era_pos_invoice/static/src/xml/era_l10n_sa_pos.xml',
         ],
     },
 
