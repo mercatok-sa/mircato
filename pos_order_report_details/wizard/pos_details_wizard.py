d38607193bfd4c6d80f9c3864a7de26b53b38bed
eranetsa <41900050+eranetsa@users.noreply.github.com>
2021-12-19 07:30:13 +0300

    Add files via upload

--- pos_order_report_details/wizard/pos_details_wizard.py
+++ pos_order_report_details/wizard/pos_details_wizard.py
@@ -15,26 +15,8 @@ class PosOrderReportWizard(models.TransientModel):
     _name = 'pos.order.report.wizard'
     _description = 'Point of Sale Order Report'
 
-    def _default_start_date(self):
-        """ Find the earliest start_date of the latests sessions """
-        # restrict to configs available to the user
-        config_ids = self.env['pos.config'].search([]).ids
-        # exclude configs has not been opened for 2 days
-        self.env.cr.execute("""
-                SELECT
-                max(start_at) as start,
-                config_id
-                FROM pos_session
-                WHERE config_id = ANY(%s)
-                AND start_at > (NOW() - INTERVAL '2 DAYS')
-                GROUP BY config_id
-            """, (config_ids,))
-        latest_start_dates = [res['start'] for res in self.env.cr.dictfetchall()]
-        # earliest of the latest sessions
-        return latest_start_dates and min(latest_start_dates) or fields.Datetime.now()
-
-    start_date = fields.Datetime(required=True, default=_default_start_date)
-    end_date = fields.Datetime(required=True, default=fields.Datetime.now)
+    start_date = fields.Date(required=True)
+    end_date = fields.Date(required=True)
     pos_config_ids = fields.Many2many('pos.config', 'pos_order_report_configs',
                                       default=lambda s: s.env['pos.config'].search([]))
 
@@ -74,101 +56,87 @@ class PosOrderReportWizard(models.TransientModel):
                       [('date_order', '>=', fields.Datetime.to_string(date_start)),
                        ('date_order', '<=', fields.Datetime.to_string(date_stop))]
                       ])
-        # if config_ids:
-        #     domain = AND([domain, [('config_id', 'in', config_ids)]])
 
         orders = self.env['pos.order'].search(domain)
 
-        for config_id in config_ids:
-            config_name = self.env['pos.config'].search([('id', '=', config_id)]).name
-            orders_ids = orders.search([('config_id', '=', config_id)])
+        if orders:
+            for config_id in config_ids:
+                config_name = self.env['pos.config'].search([('id', '=', config_id)]).name
+                orders_ids = orders.search([('config_id', '=', config_id)])
 
-            sales_order_ids = orders_ids.filtered(lambda s: len(s.refunded_order_ids) == 0)
-            sale_amount_with_tax = sum(sales_order_ids.mapped('amount_total'))
+                sales_order_ids = orders_ids.filtered(lambda s: len(s.refunded_order_ids) == 0)
+                sale_amount_with_tax = sum(sales_order_ids.mapped('amount_total'))
 
-            # ضريبة المبيعات
-            sale_order_tax = sum(sales_order_ids.mapped('amount_tax'))
-            # المبيعات
-            sale_order_amount = sale_amount_with_tax - sale_order_tax
+                # ضريبة المبيعات
+                sale_order_tax = sum(sales_order_ids.mapped('amount_tax'))
+                # المبيعات
+                sale_order_amount = sale_amount_with_tax - sale_order_tax
 
-            returns_order_ids = orders_ids.filtered(lambda s: len(s.refunded_order_ids) != 0)
-            returns_amount_with_tax = sum(returns_order_ids.mapped('amount_total'))
+                returns_order_ids = orders_ids.filtered(lambda s: len(s.refunded_order_ids) != 0)
+                returns_amount_with_tax = sum(returns_order_ids.mapped('amount_total'))
 
-            # ضريبة المردودات
-            returns_order_tax = sum(returns_order_ids.mapped('amount_tax'))
-            # المردودات
-            returns_order_amount = returns_amount_with_tax - returns_order_tax
+                # ضريبة المردودات
+                returns_order_tax = sum(returns_order_ids.mapped('amount_tax'))
+                # المردودات
+                returns_order_amount = returns_amount_with_tax - returns_order_tax
 
-            # صافي المبيعات
-            net_sales_amount = sale_order_amount + returns_order_amount
-            # صافي الضريبة
-            net_tax_amount = sale_order_tax + returns_order_tax
+                # صافي المبيعات
+                net_sales_amount = sale_order_amount + returns_order_amount
+                # صافي الضريبة
+                net_tax_amount = sale_order_tax + returns_order_tax
 
-            payment_ids = orders_ids.mapped('payment_ids')
+                payment_ids = orders_ids.mapped('payment_ids')
 
-            # النقدية
-            cash_payment = sum(payment_ids.filtered(lambda payment: payment.payment_method_id.name == "Cash").mapped('amount'))
-            # ماستر كارد
-            master_cart_payment = sum(payment_ids.filtered(lambda payment: payment.payment_method_id.name != "Cash").mapped('amount'))
-            # الإجمالي
-            total_payment = cash_payment + master_cart_payment
+                # النقدية
+                cash_payment = sum(payment_ids.filtered(lambda payment: payment.payment_method_id.name == "Cash").mapped('amount'))
+                # ماستر كارد
+                master_cart_payment = sum(payment_ids.filtered(lambda payment: payment.payment_method_id.name != "Cash").mapped('amount'))
+                # الإجمالي
+                total_payment = cash_payment + master_cart_payment
 
-            # payment_ids = self.env["pos.payment"].search([('pos_order_id', 'in', orders.ids)]).ids
-            # if payment_ids:
-            #     self.env.cr.execute("""
-            #         SELECT method.name, sum(amount) total
-            #         FROM pos_payment AS payment,
-            #              pos_payment_method AS method
-            #         WHERE payment.payment_method_id = method.id
-            #             AND payment.id IN %s
-            #         GROUP BY method.name
-            #     """, (tuple(payment_ids),))
-            #     payments = self.env.cr.dictfetchall()
-            # else:
-            #     payments = []
+                vals = {
+                    'config_name': config_name,
+                    'sale_order_amount': sale_order_amount,
+                    'returns_order_amount': returns_order_amount,
+                    'net_sales_amount': net_sales_amount,
+                    'sale_order_tax': sale_order_tax,
+                    'returns_order_tax': returns_order_tax,
+                    'net_tax_amount': net_tax_amount,
+                    'cash_payment': cash_payment,
+                    'master_cart_payment': master_cart_payment,
+                    'total_payment': total_payment,
+                }
+                res.append(vals)
+
+        if res:
+            total_sale_order_amount = total_returns_order_amount = total_net_sales_amount = 0
+            total_sale_order_tax = total_returns_order_tax = total_net_tax_amount = 0
+            total_cash_payment = total_master_cart_payment = total_total_payment = 0
+
+            for rec in res:
+                total_sale_order_amount += rec['sale_order_amount']
+                total_returns_order_amount += rec['returns_order_amount']
+                total_net_sales_amount += rec['net_sales_amount']
+                total_sale_order_tax += rec['sale_order_tax']
+                total_returns_order_tax += rec['returns_order_tax']
+                total_net_tax_amount += rec['net_tax_amount']
+                total_cash_payment += rec['cash_payment']
+                total_master_cart_payment += rec['master_cart_payment']
+                total_total_payment += rec['total_payment']
 
             vals = {
-                'config_name': config_name,
-                'sale_order_amount': sale_order_amount,
-                'returns_order_amount': returns_order_amount,
-                'net_sales_amount': net_sales_amount,
-                'sale_order_tax': sale_order_tax,
-                'returns_order_tax': returns_order_tax,
-                'net_tax_amount': net_tax_amount,
-                'cash_payment': cash_payment,
-                'master_cart_payment': master_cart_payment,
-                'total_payment': total_payment,
+                'config_name': 'Totals',
+                'sale_order_amount': total_sale_order_amount,
+                'returns_order_amount': total_returns_order_amount,
+                'net_sales_amount': total_net_sales_amount,
+                'sale_order_tax': total_sale_order_tax,
+                'returns_order_tax': total_returns_order_tax,
+                'net_tax_amount': total_net_tax_amount,
+                'cash_payment': total_cash_payment,
+                'master_cart_payment': total_master_cart_payment,
+                'total_payment': total_total_payment,
             }
             res.append(vals)
-
-        total_sale_order_amount = total_returns_order_amount = total_net_sales_amount = 0
-        total_sale_order_tax = total_returns_order_tax = total_net_tax_amount = 0
-        total_cash_payment = total_master_cart_payment = total_total_payment = 0
-
-        for rec in res:
-            total_sale_order_amount += rec['sale_order_amount']
-            total_returns_order_amount += rec['returns_order_amount']
-            total_net_sales_amount += rec['net_sales_amount']
-            total_sale_order_tax += rec['sale_order_tax']
-            total_returns_order_tax += rec['returns_order_tax']
-            total_net_tax_amount += rec['net_tax_amount']
-            total_cash_payment += rec['cash_payment']
-            total_master_cart_payment += rec['master_cart_payment']
-            total_total_payment += rec['total_payment']
-
-        vals = {
-            'config_name': 'Totals',
-            'sale_order_amount': total_sale_order_amount,
-            'returns_order_amount': total_returns_order_amount,
-            'net_sales_amount': total_net_sales_amount,
-            'sale_order_tax': total_sale_order_tax,
-            'returns_order_tax': total_returns_order_tax,
-            'net_tax_amount': total_net_tax_amount,
-            'cash_payment': total_cash_payment,
-            'master_cart_payment': total_master_cart_payment,
-            'total_payment': total_total_payment,
-        }
-        res.append(vals)
 
         return res
 
