3b45f7735c45f2eab1f740d8ada63c96d1acbe46
said kraim <dev.said.kraim@gmail.com>
2022-01-18 15:36:21 +0100

    add module mircato account

--- mircato_account/__manifest__.py
+++ mircato_account/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Mircato Account",
-    "version": "15.0.0.1",
+    "version": "15.0.0.2",
     "category": "Accounting",
     'description': """
        Mircato Accounting
