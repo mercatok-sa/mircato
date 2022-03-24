72cb17e10e764d77f5d3f4ab02be25eeacce873d
tamakunsa <92423483+tamakunsa@users.noreply.github.com>
2022-03-24 17:39:27 +0300

    Delete عهد directory

--- عهد/petty_cash_extention/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_invoice_inh
-from . import petty_cash_inh
-from . import petty_cash
-from . import res_users_inh
-
--- عهد/petty_cash_extention/models/res_users_inh.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, exceptions
-from datetime import date, datetime, time, timedelta
-from odoo.fields import Date, Datetime
-class Users(models.Model):
-    _inherit = 'res.users'
-
-    show_emps_petty_cashes = fields.Boolean('Show emps Petty Cashes')
