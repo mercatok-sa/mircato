8b67f2c9240880f9748a68c91a3118eb3b1f63cc
tamakunsa <92423483+tamakunsa@users.noreply.github.com>
2022-03-28 18:33:27 +0300

    Add files via upload

---
+++ era_pos_item_count/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ERA Pos Item count',
+    'version': '15.0.1.0',
+    'category': 'Point of Sale',
+    'sequence': 6,
+    'auther': 'ERA Group',
+    'email': 'info@era.net.sa',
+    'website': 'https://era.net.sa',
+    'summary': 'Pos Item count module show you number of products in cart.',
+    'description': """ This module show you number of products in cart.""",
+    'depends': ['point_of_sale'],
+    'data': [
+    ],
+    'images': [
+    ],
+
+     'assets': {
+        'web.assets_qweb': [
+            'era_pos_item_count/static/src/xml/pos.xml',
+        ],
+        'point_of_sale.assets': [
+            'era_pos_item_count/static/src/js/pos.js',
+        ]
+    },
+
+    'installable': True,
+    'auto_install': False,
+
+}
+
