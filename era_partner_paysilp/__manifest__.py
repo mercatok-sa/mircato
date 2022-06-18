e19e3fffae113ef968ef59a1992850b9289528fd
SaidKraimDev <76952459+SaidKraimDev@users.noreply.github.com>
2022-06-18 15:36:41 +0100

    Merge main with last correction

---
+++ era_partner_paysilp/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ERA Partner in paysilps journal entry',
+    'version': '15.0.1.0',
+    'category': 'hr',
+    'sequence': 6,
+    'auther': 'ERA Group',
+    'email': 'info@era.net.sa',
+    'website': 'https://era.net.sa',
+    'summary': 'Partner in paysilps journal entry.',
+    'description': """ This module add partner in paysilps journal entry.""",
+    'depends': ['hr', 'hr_payroll'],
+    'data': [
+        'views/hr_employee.xml',
+    ],
+    'images': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+
+}
+
---
+++ era_partner_paysilp/models/hr_payslip.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+
+class HrPayslipAcc(models.Model):
+    _inherit = 'hr.payslip'
+
+    def _prepare_line_values(self, line, account_id, date, debit, credit):
+        value = super(HrPayslipAcc, self)._prepare_line_values(line, account_id, date, debit, credit)
+        if line.code == 'NET':
+            value['partner_id'] = line.slip_id.employee_id.address_home_id.id
+        return value
+
+    def _get_existing_lines(self, line_ids, line, account_id, debit, credit):
+        if line.code == 'NET':
+            return False
+        else:
+            return super(HrPayslipAcc, self)._get_existing_lines(line_ids, line, account_id, debit, credit)
+
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    _sql_constraints = [
+        ('unique_address_home_id', 'UNIQUE(address_home_id)', 'No duplication of employee address')
+    ]
