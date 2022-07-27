00b19285d256a16278100f5c848ede997ba9196d
AbdulghaniFawzi <abdelgahnifawzi@gmail.com>
2022-07-27 17:50:55 +0300

    after modify by khabab

--- era_partner_paysilp/__manifest__.py
+++ era_partner_paysilp/__manifest__.py
@@ -2,14 +2,14 @@
 {
     'name': 'ERA Partner in paysilps journal entry',
     'version': '15.0.2.0',
-    'category': 'hr',
-    'sequence': 6,
+    'category': 'Human Resources/Payroll',
+    'license': 'OPL-1',
     'auther': 'ERA Group',
     'email': 'info@era.net.sa',
     'website': 'https://era.net.sa',
     'summary': 'Partner in paysilps journal entry.',
     'description': """ This module add partner in paysilps journal entry.""",
-    'depends': ['hr_payroll'],
+    'depends': ['hr_payroll_account'],
     'data': [
         'views/hr_employee.xml',
     ],
--- era_partner_paysilp/models/hr_payslip.py
+++ era_partner_paysilp/models/hr_payslip.py
@@ -7,15 +7,14 @@ class HrPayslipAcc(models.Model):
 
     def _prepare_line_values(self, line, account_id, date, debit, credit):
         value = super(HrPayslipAcc, self)._prepare_line_values(line, account_id, date, debit, credit)
-        if line.code == 'NET':
-            value['partner_id'] = line.slip_id.employee_id.address_home_id.id
+        value['partner_id'] = line.slip_id.employee_id.address_home_id.id
         return value
 
-    def _get_existing_lines(self, line_ids, line, account_id, debit, credit):
-        if line.code == 'NET':
-            return False
-        else:
-            return super(HrPayslipAcc, self)._get_existing_lines(line_ids, line, account_id, debit, credit)
+    # def _get_existing_lines(self, line_ids, line, account_id, debit, credit):
+    #     if line.code == 'NET':
+    #         return False
+    #     else:
+    #         return super(HrPayslipAcc, self)._get_existing_lines(line_ids, line, account_id, debit, credit)
 
 
 class HrEmployee(models.Model):
