61cbe5274bb4021fa24192c3cd712fe201ba3dab
tariqlab <tariq.bstc@gmail.com>
2023-01-01 11:32:10 +0300

    update addons 9

--- tn_product_barcode/models/update_product.py
+++ tn_product_barcode/models/update_product.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models, _
 class POSConfig(models.Model):
     _inherit = 'product.template'
 
-    barcode = fields.char('')
+    barcode = fields.Char('')
     def _aupdate_barcode(self):
         for ref in self:
             ref.barcode=ref.default_code
