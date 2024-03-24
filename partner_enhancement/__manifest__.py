9d664408f48e6a8ddce0fe786263bd448507414b
Omnya Rashwan <o.reshwan@tatayab.com>
2024-03-24 02:03:58 +0200

    [ADD] partner_enhancement: Add domain on customer and vendor in sales and purchase

---
+++ partner_enhancement/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Partner Enhancement",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "Omnya Rashwan",
+    'category': 'Partner',
+    'depends': ['base'],
+    'data': [
+        'views/purchase_order_inh_view.xml',
+        'views/sale_order_inh_view.xml'
+    ],
+}
